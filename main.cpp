7983f60dbf0885f7601d5502304a7d4cfd1cc2b2
isidorostsa <tsa.isidoros@gmail.com>
2020-10-06 23:04:21 +0300

    removed drawWalls boolean, always draw border walls

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #define HEIGHT 600
 #define RAY_NUM 100
 #define WALL_NUM 20
-#define PI  3.14159265358979323846
+#define PI  3.1415926535897932384
 
 #include "ray.cpp"
 #include "wall.cpp"
@@ -20,14 +20,13 @@ void MakeRays(std::vector<Ray>& rayArray, const sf::Vector2f& mousePosition2f, s
 }
 
 void MakeWalls(std::vector<Wall>& wallArray, size_t size, bool wallsOn = true){
-    if(wallsOn){
-        wallArray.push_back(Wall(sf::Vector2f(0.f, 0.f), sf::Vector2f((float)WIDTH, 0.f)));
-        wallArray.push_back(Wall(sf::Vector2f((float)WIDTH, 0.f), sf::Vector2f((float)WIDTH, (float)HEIGHT)));
-        wallArray.push_back(Wall(sf::Vector2f((float)WIDTH, (float)HEIGHT), sf::Vector2f(0.f, (float)WIDTH)));
-        wallArray.push_back(Wall(sf::Vector2f(0.f, (float)WIDTH), sf::Vector2f(0.f, 0.f)));
-    }
-    
-    for(int i = 0; i < size; i++){
+    //draw the border walls
+    wallArray.push_back(Wall(sf::Vector2f(0.f, 0.f), sf::Vector2f((float)WIDTH, 0.f)));
+    wallArray.push_back(Wall(sf::Vector2f((float)WIDTH, 0.f), sf::Vector2f((float)WIDTH, (float)HEIGHT)));
+    wallArray.push_back(Wall(sf::Vector2f((float)WIDTH, (float)HEIGHT), sf::Vector2f(0.f, (float)WIDTH)));
+    wallArray.push_back(Wall(sf::Vector2f(0.f, (float)WIDTH), sf::Vector2f(0.f, 0.f)));
+
+    for(size_t i = 0; i < size; i++){
 
         float x1 = WIDTH * (float)rand() / RAND_MAX;
         float y1 = HEIGHT * (float)rand() / RAND_MAX;
@@ -49,6 +48,7 @@ int main(int argc, char* argv[]){
     sf::ContextSettings settings;
     settings.antialiasingLevel = 100;
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML", sf::Style::Default, settings);
+    window.setFramerateLimit(200);
 
     std::vector<Wall> wallArray;
     MakeWalls(wallArray, WALL_NUM);
@@ -56,12 +56,14 @@ int main(int argc, char* argv[]){
     std::vector<Ray> rayArray;
     MakeRays(rayArray, sf::Vector2f(0.f, 0.f), RaysAmmount, 500.f);
 
-    size_t frameCount = 0; 
-    std::vector<float> last100fps; 
+    size_t frameCount = 0;
+    float lastAvgFps = 200;
+    std::vector<float> last100fps;
 
     while(window.isOpen()){
         const sf::Vector2f mousePosition2f = (sf::Vector2f)sf::Mouse::getPosition(window);
         sf::Event event;
+
         while(window.pollEvent(event)){
             switch (event.type)
             {
@@ -81,8 +83,8 @@ int main(int argc, char* argv[]){
 
         for(Ray& ray : rayArray){
             ray[0].position = mousePosition2f;
-            if(ray.ProjectOnto(wallArray))
-                window.draw(ray);
+            ray.ProjectOnto(wallArray);
+            window.draw(ray);
         }
 
         window.display();
@@ -92,12 +94,13 @@ int main(int argc, char* argv[]){
         if(frameCount == 100){
             // calculate average fps over last100fps
 
-            float sum = 0; // hope we don't overflow 
+            double sum = 0; // hope we don't overflow :P
             for(float _fps : last100fps){
                 sum += _fps;
             }
+            float avgFps = sum / 100.f;
 
-            std::cout << "average fps over the last 100 frames : " << sum / 100.f << std::endl;
+            std::cout << "average fps over the last 100 frames : " << avgFps << std::endl;
             
             frameCount = 0; 
             last100fps.clear();
